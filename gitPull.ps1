e151bd560795cadfdb1d59484550e01f78d80f9c
Deependra Mehrotra <mehrotra.deeprndra@gmail.com>
2020-04-01 09:22:00 -0500

    Automate git pull

---
+++ gitPull.ps1
@@ -0,0 +1,36 @@
+﻿param([string]$repoLocalpath)
+
+$repoLocalpath = Read-Host -Prompt 'Input your GIT Repo' 
+
+if($repoLocalpath -eq "")
+{
+    Write-Host "You gotta provide path" -Path -Level Error  -ForegroundColor Red
+    #return
+}
+else {
+    Write-Host "You entered '$repoLocalpath' " -ForegroundColor DarkGreen 
+}
+
+#You can directly hardcode your repo root path in case you want to avoid inputing path everytime 
+#$repoLocalpath = "C:\Code";
+
+Write-Host "Starting with root: $repoLocalpath" 
+
+$dir = Get-ChildItem $repoLocalpath | Where-Object {$_.PSISContainer} | select-object FullName
+
+foreach ($d in $dir) 
+{
+    Write-Host "git pull --all -> $($d.FullName)"  -ForegroundColor Green
+    Set-Location $d.FullName;
+    git pull -v --all
+   # Write-Host 
+
+    #build web and portal
+    #find if there are any difference for project then build it 
+}
+
+Set-Location $PSScriptRoot;
+
+Write-Host "Done"  
+
+#https://www.freecodecamp.org/forum/t/git-pull-how-to-override-local-files-with-git-pull/13216
